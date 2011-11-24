f984d15f9ddc1a6d678c34997b3905f7d8761605
Anton Versal <ant.ver@gmail.com>
2011-11-24 21:59:18 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cotologue::Application.config.secret_token = '0309b6b79f1288ebcbd072f394d7798c0199936ab7c74dbe727cedd623e766ed29c0c9c6986b3efce1b2f8ec82f92c2010c90cbffb89bca09c2c39c4b99b8260'
