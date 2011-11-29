95bd635d129d3cf382f4006a66c5e3f3607c7d5c
Anton Versal <ant.ver@gmail.com>
2011-11-29 23:07:09 +0200

    corrected products tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_db_column(:name).of_type(:string).with_options(:null => false) }
+  it { should have_db_column(:description).of_type(:text) }
+  it { should have_db_column(:section_id).of_type(:integer) }
+
+  it { should have_db_index(:section_id) }
+
+  it { should validate_presence_of(:name) }
+
 end
