d3625706fe29087afdc429e1cc6e0d6fc3bb938b
Anton Versal <ant.ver@gmail.com>
2011-11-29 22:57:14 +0200

    updated model template

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -10,6 +10,7 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name %>
 <% end -%>
+
   # Delegates
 
   # Validations: presence > by type > validates
