58f1cd748a62ba5605973276bacd233882f00b7d
Anton Versal <ant.ver@gmail.com>
2011-12-01 08:44:10 +0200

    fixed routing tests

--- spec/routing/values_routing_spec.rb
+++ spec/routing/values_routing_spec.rb
@@ -4,31 +4,27 @@ describe ValuesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/values").should route_to("values#index")
+      get("/filters/1/values").should route_to("values#index", filter_id: "1")
     end
 
     it "routes to #new" do
-      get("/values/new").should route_to("values#new")
-    end
-
-    it "routes to #show" do
-      get("/values/1").should route_to("values#show", :id => "1")
+      get("/filters/1/values/new").should route_to("values#new", filter_id: "1")
     end
 
     it "routes to #edit" do
-      get("/values/1/edit").should route_to("values#edit", :id => "1")
+      get("/filters/1/values/1/edit").should route_to("values#edit", id: "1", filter_id: "1")
     end
 
     it "routes to #create" do
-      post("/values").should route_to("values#create")
+      post("/filters/1/values").should route_to("values#create", filter_id: "1")
     end
 
     it "routes to #update" do
-      put("/values/1").should route_to("values#update", :id => "1")
+      put("/filters/1/values/1").should route_to("values#update", id: "1", filter_id: "1")
     end
 
     it "routes to #destroy" do
-      delete("/values/1").should route_to("values#destroy", :id => "1")
+      delete("/filters/1/values/1").should route_to("values#destroy", id: "1", filter_id: "1")
     end
 
   end
