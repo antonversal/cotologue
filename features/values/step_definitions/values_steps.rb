00eca8561613bde317c125307c35e578fcc4455f
Anton Versal <ant.ver@gmail.com>
2011-12-01 08:12:51 +0200

    values steps

---
+++ features/values/step_definitions/values_steps.rb
@@ -0,0 +1,15 @@
+Given /^I am on new value page$/ do
+  visit new_filter_value_path(Filter.last)
+end
+
+When /^I fill value details correctly$/ do
+  fill_in "Name", with: "CPU"
+end
+
+Then /^I should be on values page$/ do
+  current_path.should eq(filter_values_path(Filter.last))
+end
+
+When /^I should see value errors$/ do
+  find(:simple_form_error, "value_name").text.should eq("can't be blank")
+end
