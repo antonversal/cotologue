fcc188d25ac37c47f87ddb03e395b707b51f5c52
Anton Versal <ant.ver@gmail.com>
2011-12-01 08:27:40 +0200

    added validation to value model

--- spec/models/value_spec.rb
+++ spec/models/value_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Value do
   it { should have_db_column(:name).of_type(:string).with_options(null: false) }
+  it { should have_db_column(:filter_id).of_type(:integer).with_options(null: false) }
 
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:filter) }
+
 end
